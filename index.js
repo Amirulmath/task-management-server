c4cca898621139269ce5103c42a144bdd6c73168
Amirulmath <amirulmath760@gmail.com>
2022-12-29 15:49:15 +0600

    Daily task & Add task API created

--- index.js
+++ index.js
@@ -15,6 +15,28 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xjhllfd.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        const taskCollection = client.db('taskManagement').collection('myTask');
+        const mediaTaskCollection = client.db('taskManagement').collection('mediaTask');
+        
+        app.post('/mytasks', async (req, res) => {
+            const myTasks = req.body;
+            const result = await taskCollection.insertOne(myTasks);
+            res.send(result);
+        });
+
+        app.post('/mediatasks', async (req, res) => {
+            const media = req.body;
+            const result = await mediaTaskCollection.insertOne(media);
+            res.send(result);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err));
 
 app.get('/', async (req, res) => {
     res.send('Task Management server is running');
